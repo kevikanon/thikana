7440904eb4fcf8061e9c80012fe7184b98381527
kevikanon <kevikanon@gmail.com>
2021-06-20 01:56:00 +0600

    Base Classes
    Created base classes for project.

---
+++ src/Thikana.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Kevikanon;
+
+class Thikana
+{
+
+
+}
---
+++ src/ThikanaServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Kevikanon;
+
+use Illuminate\Support\ServiceProvider;
+
+class ThikanaServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
